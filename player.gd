b3b08ca86f926e2314866d9074bc202031c14834
JCBruhman <justincanela8@gmail.com>
2026-07-07 15:56:08 -0400

    Lots of boss art and minor city changes

--- player.gd
+++ player.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 
 var speed = 500.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("run") and is_on_floor():
-		speed = 1200.0
-	elif not Input.is_action_pressed("run") or not is_on_floor():
+	if Input.is_action_pressed("run"):
+		speed = 1000.0
+	elif not Input.is_action_pressed("run"):
 		speed = 500.0
 	
 	# Add the gravity.
