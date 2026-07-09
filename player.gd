eb5bb5c72fb3a7d1fea074276491d6475734a440
JCBruhman <justincanela8@gmail.com>
2026-07-09 16:27:07 -0400

    We are so fucked (Attacks and health still not set up, missing sword and gun attacks, prolly not doing dark variant)

--- player.gd
+++ player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 700.0
+const SPEED = 800.0
 const JUMP_VELOCITY = -600.0
 const DASHSPEED = 1300
 var canDash = true
+var left
 
 func _physics_process(delta: float) -> void:
 		
@@ -22,9 +23,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("left"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("Run")
+		left = true
 	elif Input.is_action_pressed("right"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("Run")
+		left = false
 	else:
 		$AnimatedSprite2D.play("Idle")
 	
@@ -33,17 +36,35 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 		
-	elif Input.is_action_just_pressed("run") and canDash:
-		velocity.x = direction * DASHSPEED
-		await get_tree().create_timer(1).timeout 
-		canDash = false
+	#elif Input.is_action_just_pressed("run") and canDash:
+		#velocity.x = direction * DASHSPEED
+		#await get_tree().create_timer(1).timeout 
+		#canDash = false
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if not canDash:
-		await get_tree().create_timer(1).timeout 
-		canDash = true
+	#if not canDash:
+		#await get_tree().create_timer(1).timeout 
+		#canDash = true
+	
+	
+	var bulletSprite = Sprite2D.new()
+	bulletSprite.texture = load("res://Art/Player/Bullet.png")
+#	-1373, 1838
+	if Input.is_action_just_pressed("shoot"):
+		
+		bulletSprite.scale = Vector2(.3, .3)
+		if left:
+			bulletSprite.global_position = Vector2(self.global_position.x, self.global_position.y)
+			bulletSprite.rotation = PI
+		else:
+			bulletSprite.global_position = Vector2(self.global_position.x, self.global_position.y)
+		add_child(bulletSprite)
+			
+		var tween = get_tree().create_tween()
+		tween.tween_property(bulletSprite, "position", Vector2(self.global_position.x * 1.2, self.global_position.y), 0.40)
+		tween.tween_callback(bulletSprite.hide.bind())
 		
 
 	move_and_slide()
